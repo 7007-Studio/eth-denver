ca7d004ac24bb2361f24302915a5f1264b230841
tina1998612 <tleeae@connect.ust.hk>
2024-02-28 20:11:31 -0700

    eth storage waiting for testnet token

---
+++ ethstorage.js
@@ -0,0 +1,40 @@
+import ethfs from "ethfs-sdk";
+import fs from "fs";
+import ethers from "ethers";
+import dotenv from "dotenv";
+dotenv.config();
+
+// callback, can be null
+const onProgress = (chunkIndex, totalChunk, fileName) => {};
+const onSuccess = (fileName) => {
+  console.log("success");
+};
+const onError = (message) => {
+  // console.log("error", message);
+};
+
+const uploadFile = async (filePath, fileName) => {
+  const fileStat = fs.statSync(filePath);
+  const content = fs.readFileSync(filePath);
+  const directoryPath = "img/" + fileName;
+
+  const rpc = "https://galileo.web3q.io:8545";
+  const privateKey = process.env.PRIVATE_KEY;
+  const provider = new ethers.providers.JsonRpcProvider(rpc);
+  const signer = new ethers.Wallet(privateKey, provider);
+  const directoryAddress = await ethfs.createDirectory(signer);
+  console.log(directoryAddress);
+
+  await ethfs.upload(
+    signer,
+    directoryAddress,
+    directoryPath,
+    fileStat.size,
+    content,
+    onProgress,
+    onSuccess,
+    onError
+  );
+};
+
+uploadFile("img/w644.jpeg", "file");
