7c9f0d9c0e30733ed841ebb3f21d47ae75f12fa8
tina1998612 <tleeae@connect.ust.hk>
2024-02-28 19:17:09 -0700

    d

--- music.py
+++ music.py
@@ -1,6 +1,5 @@
 from bark import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
-from IPython.display import Audio
 
 # download and load all models
 preload_models()
@@ -14,5 +13,3 @@ audio_array = generate_audio(text_prompt)
 # save audio to disk
 write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
   
-# play text in notebook
-Audio(audio_array, rate=SAMPLE_RATE)
